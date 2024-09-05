e3e47936371952db005f626e9ba9e02c71e98095
Olena-Synytsia <alenasinitsa85@gmail.com>
2024-09-05 14:18:40 +0300

    components

---
+++ src/components/Description/Description.jsx
@@ -0,0 +1,7 @@
+import s from "./Description.module.css";
+
+const Description = () => {
+  return <div className={s.container}>Description</div>;
+};
+
+export default Description;
---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,7 @@
+import s from './Feedback.module.css'
+
+const Feedback = () => {
+  return <div className={s.container}>Feedback</div>;
+};
+
+export default Feedback;

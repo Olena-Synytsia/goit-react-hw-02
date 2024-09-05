5b15e9bb63af8e074b7b1552e7cbbcd1cc36eef5
Olena-Synytsia <alenasinitsa85@gmail.com>
2024-09-05 15:13:11 +0300

    Options start

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,7 +1,14 @@
 import s from "./Options.module.css";
 
 const Options = () => {
-  return <div className={s.container}>Options</div>;
+  return (
+    <div className={s.container}>
+      <button className={s.button}>Good</button>
+      <button className={s.button}>Neutral</button>
+      <button className={s.button}>Bad</button>
+      <button className={s.button}>Reset</button>
+    </div>
+  );
 };
 
 export default Options;
